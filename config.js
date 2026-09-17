87b94025e369e9b22799c7aa24480f6b5b7e0e46
Lucaxrodrigues <lucasrodrigues6516@gmail.com>
2026-09-17 16:22:19 -0300

    Gws

---
+++ config.js
@@ -0,0 +1,5 @@
+window.ALFA_CONFIG = {
+  "supabaseUrl": "https://lwnmbzdihjtlyluhktqr.supabase.co",
+  "supabaseKey": "sb_publishable_XJ9GgpIUs_9e74iBUpRTxQ_F3PbbEsr",
+  "supportEmail": "guiasuporteoficial@gmail.com"
+};
